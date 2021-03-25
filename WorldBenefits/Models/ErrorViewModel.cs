b9cf57351b2643e47932419b8feaaf282c65e7fd
John Haythorn <john.haythorn@bglgroup.co.uk>
2021-03-25 20:02:13 +0000

    first benefit

---
+++ WorldBenefits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorldBenefits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
