3c78fddb4fbefc9e0664e70caf05e5922010db2a
John Haythorn <john.haythorn@bglgroup.co.uk>
2021-04-20 09:09:39 +0100

    stop looking at my stuff damian

--- WorldBenefits/Controllers/HomeController.cs
+++ WorldBenefits/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace WorldBenefits.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ WorldBenefits/Controllers/PlacesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldBenefits.Controllers
+{
+    public class PlacesController : Controller
+    {
+        // GET: PlacesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PlacesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PlacesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlacesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlacesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlacesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlacesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlacesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Places()
+        {
+            return View();
+        }
+    }
+}
